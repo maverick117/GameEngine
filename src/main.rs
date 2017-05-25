3bf6665b3677475ad2a7b258339b524d35b64365
TCMOOO <583806447@qq.com>
2017-05-25 21:38:37 +0800

    display a window

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ mod render;
 mod input;
 mod model;
 
+#[macro_use]
+extern crate gfx;
+extern crate gfx_window_glutin;
+extern crate glutin;
+
+pub type ColorFormat = gfx::format::Rgba8;
+pub type DepthFormat = gfx::format::DepthStencil;
+
 pub struct Msg {
 
 }
@@ -13,6 +21,23 @@ pub trait System{
 
 }
 
-fn main() {
-    println!("Hello, world!");
+pub fn main() {
+    let events_loop = glutin::EventsLoop::new();
+    let builder = glutin::WindowBuilder::new()
+        .with_title("Game window".to_string())
+        .with_dimensions(1024, 768)
+        .with_vsync();
+    let (window, mut device, mut factory, main_color, mut main_depth) =
+        gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder, &events_loop);
+    let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+
+    let mut running = true;
+    while running {
+        // fetch events
+        
+
+        // draw a frame
+        encoder.flush(&mut device);
+        window.swap_buffers().unwrap();
+    }
 }
