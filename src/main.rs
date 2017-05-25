4bd6a040d5ffcc14ebd6a76f7b69e2d6258d945a
Andrew Jianzhong Liu <liujzh@shanghaitech.edu.cn>
2017-05-25 20:31:30 +0800

    Added placeholders for all modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod console;
+mod render;
+mod input;
+mod model;
+
+pub struct Msg {
+
+}
+
+pub trait System{
+    fn handle_message(msg: Msg);
+
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
