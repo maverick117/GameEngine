4e0e4c5bcc29756cc31979b2ece60f527fb13743
Andrew Jianzhong Liu <liujzh@shanghaitech.edu.cn>
2017-05-25 21:55:49 +0800

    Outline updated

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,47 @@ mod render;
 mod input;
 mod model;
 
-pub struct Msg {
+use std::sync::mpsc::*;
+use std::thread;
+use std::sync::mpsc::channel;
 
+use input::InputSystem;
+
+pub enum SystemMsg {
+    SysInit,
+    SysHalt,
+    SysUpdate,
+    SysFlush,
 }
 
-pub trait System{
-    fn handle_message(msg: Msg);
+pub struct Msg {}
 
+pub trait System {
+    fn init(&self);
+    fn main_loop(&mut self);
+    fn add_tx(&mut self, msg_tx: Sender<Msg>);
+    fn set_rx(&mut self, msg_rx: Receiver<Msg>);
+}
 
+fn spawn_systems<T>(mut sys: T)
+    where T: System
+{
+    println!("Spawning systems");
+    sys.init();
+    sys.main_loop();
 }
 
 fn main() {
-    println!("Hello, world!");
+
+    println!("Welcome to Game Engine. Initializing all systems");
+
+
+    let input_system: InputSystem = InputSystem::new();
+
+
+    let input_handle = thread::spawn(move || spawn_systems(input_system));
+
+    input_handle.join().unwrap();
+
+
 }
