edb9b345076b3d9c441d0bc06d3a7784be9a990b
Andrew Jianzhong Liu <liujzh@shanghaitech.edu.cn>
2017-05-26 17:07:51 +0800

    Added timestamp for console

--- src/console.rs
+++ src/console.rs
@@ -1,5 +1,6 @@
 
 use std::sync::mpsc::*;
+use std::time::SystemTime;
 
 use super::System;
 use super::Msg;
@@ -8,17 +9,22 @@ use super::*;
 pub struct ConsoleSystem {
     msg_tx: Vec<Sender<Msg>>,
     msg_rx: Receiver<Msg>,
+    startup_time: SystemTime,
 }
 
 impl System for ConsoleSystem {
     fn init(&mut self) {
         println!("Console Running.");
+        self.startup_time = SystemTime::now();
     }
 
     fn main_loop(&mut self) {
         loop {
             let msg = self.msg_rx.recv().unwrap();
-            println!("Console Msg Received: {:?}", msg);
+            let elapsed_time = SystemTime::now()
+                .duration_since(self.startup_time)
+                .expect("duration_since failed.");
+            println!("{:?}: {:?}", elapsed_time, msg);
             if let MsgContent::System(SystemMsg::SysHalt) = msg.content {
                 break;
             }
@@ -31,6 +37,7 @@ impl ConsoleSystem {
         ConsoleSystem {
             msg_tx: msg_tx,
             msg_rx: msg_rx,
+            startup_time: SystemTime::now(),
         }
     }
 }
