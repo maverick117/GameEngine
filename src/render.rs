f3d94f9918126e4677014693085862716e920ffc
zhouza <zeanzhou@qq.com>
2017-05-28 04:06:41 +0800

    add missing semicolon in frag shader

--- src/render.rs
+++ src/render.rs
@@ -105,7 +105,7 @@ impl RenderSystem {
                     #version 330
                     // to implement
                     void main() {
-                        gl_FragColor = vec4(0.4, 0.3, 0.05, 1.0)
+                        gl_FragColor = vec4(0.4, 0.3, 0.05, 1.0);
                     }
                 ",
             },
